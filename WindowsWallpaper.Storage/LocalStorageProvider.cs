31dc9872eac7b3c3c768a6f731309fcec1fc603d
Sy LK <gemini02061994@outlook.com>
2017-07-29 03:32:51 +0700

    Split setting tab and define Ilocalstorage interface

---
+++ WindowsWallpaper.Storage/LocalStorageProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsWallpaper.Domain;
+
+namespace WindowsWallpaper.Storage
+{
+    public class LocalStorageProvider : ILocalstorageProvider
+    {
+        public void Set<T>(LocalStorageKeys key, T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGet<T>(LocalStorageKeys key, out T value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WindowsWallpaper.UWP/ViewModels/SettingTabViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsWallpaper.UWP.ViewModels
+{
+    public class SettingTabViewModel : BaseViewModel
+    {
+        private bool _autoUpdateBackgroundState = false;
+        public bool AutoUpdateBackgroundState
+        {
+            get { return _autoUpdateBackgroundState; }
+            set
+            {
+                if (_autoUpdateBackgroundState != value)
+                {
+                    Dictionary<int, int> d = new Dictionary<int, int>();
+                    _autoUpdateBackgroundState = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
