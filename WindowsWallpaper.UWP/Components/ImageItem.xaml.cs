a64e1b36818e7bcd472a03d750210159f2969fba
Sy LK <gemini02061994@outlook.com>
2017-08-26 13:04:16 +0700

    Fix last image not loading

--- WindowsWallpaper.UWP/Components/ImageItem.xaml.cs
+++ WindowsWallpaper.UWP/Components/ImageItem.xaml.cs
@@ -19,35 +19,10 @@ namespace WindowsWallpaper.UWP.Components
 {
     public sealed partial class ImageItem : UserControl
     {
-        ViewModels.ImageItemViewModel vm;
-
-        public static readonly DependencyProperty ImageSourceProperty =
-        DependencyProperty.Register("ImageSource", typeof(Domain.Entities.ImageSource), typeof(ImageItem), new PropertyMetadata(null));
-
-        public Domain.Entities.ImageSource ImageSource
-        {
-            get
-            {
-                return (Domain.Entities.ImageSource)GetValue(ImageSourceProperty);
-            }
-            set
-            {
-                SetValue(ImageSourceProperty, value);
-            }
-        }
 
         public ImageItem()
         {
             this.InitializeComponent();
-            vm = ViewModels.ImageItemViewModel.Create(null);
-        }
-
-        private void UserControl_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
-        {
-            if (this.DataContext is Domain.Entities.ImageSource)
-            {
-                this.DataContext = ViewModels.ImageItemViewModel.Create(this.DataContext as Domain.Entities.ImageSource);
-            }
         }
     }
 }
